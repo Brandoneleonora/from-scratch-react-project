3109bf7eb86ee5e23b7a209e37c6fbb05d73c25a
Brandon Eleonora <brandoneleonora38@gmail.com>
2023-07-02 17:45:51 -0400

    navigation buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, {useState, useEffect} from "react";
 import { Route, Switch} from 'react-router-dom'
 import Home from "./Home";
+import NavBar from "./NavBar";
+
 function App() {
   return (
    <div>
+    <NavBar />
     <Switch>
       <Route exact path = '/'>
         <Home />
